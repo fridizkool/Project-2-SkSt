79d928310f2a671c8195e8a1af9e6ad22a254052
fridizkool <fsorgtaylor@gmail.com>
2024-04-10 14:44:24 -0400

    Init frontend and backend

---
+++ Backend/taxprep/src/main/java/com/skillstorm/taxprep/TaxprepApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.taxprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxprepApplication.class, args);
+	}
+
+}
