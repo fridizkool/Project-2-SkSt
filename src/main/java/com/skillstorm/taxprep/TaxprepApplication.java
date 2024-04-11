dd345790d176f81ec6ed63bbad47ec37171cf1b4
Kaiwen Tsou <kktsou@ucsd.edu>
2024-04-11 14:11:40 -0700

    Fixed build

--- src/main/java/com/skillstorm/taxprep/TaxprepApplication.java
+++ src/main/java/com/skillstorm/taxprep/TaxprepApplication.java
@@ -2,8 +2,6 @@ package com.skillstorm.taxprep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class TaxprepApplication {	
 	public static void main(String[] args) {
--- src/main/react/src/App.tsx
+++ src/main/react/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Counter/>
-      {/* <LandingPage/> */}
+      <LandingPage/>
       <InternationalizationHello/>
     </>
   )
