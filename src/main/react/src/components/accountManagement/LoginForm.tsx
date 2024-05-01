f2538d289e6c4f4e674766741eeba8c635505d69
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-01 13:34:08 -0700

    Removed empty comments

--- src/main/react/src/components/accountManagement/LoginForm.tsx
+++ src/main/react/src/components/accountManagement/LoginForm.tsx
@@ -1,46 +1,9 @@
 import { Fieldset, Label, TextInput, Button} from '@trussworks/react-uswds';
 import { useState } from 'react';
 import { Form } from 'react-router-dom';
-// import { queryAuthStatus } from '../../service/authService';
 
 const LoginForm = () => {
     const [showPassword, setShowPassword] = useState(false);
-    // const navigate = useNavigate();
-    
-    // const submitInfo = async (event: FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault(); 
-        
-    //     const formData = new FormData(event.currentTarget); // Create FormData object from the form
-        
-    //     try {
-    //         //TODO Add CSRF token to header
-    //         const headers = new Headers();
-
-    //         const response = await fetch('/login', {
-    //             method: 'POST',
-    //             headers: headers,
-    //             body: formData
-    //         });
-
-    //         if (response.status === 200) {
-    //             // Check authentication status after successful login
-    //             const authStatus = await queryAuthStatus();
-    //             if (authStatus.authenticated) {
-    //                 // Navigate to the authenticated user's dashboard or home page
-    //                 navigate('/');
-                    
-    //             } else {
-    //                 // Handle case where user is not authenticated after login
-    //                 navigate('/login?error=true');
-    //                 console.error('User is not authenticated after login');
-    //             }
-    //         } else {
-    //             // Handle other status codes (e.g., login failed)
-    //         }
-    //     } catch (error) {
-    //         console.error('Error submitting form:', error);
-    //     }
-    // };
 
     return (
         <Form method="post">
