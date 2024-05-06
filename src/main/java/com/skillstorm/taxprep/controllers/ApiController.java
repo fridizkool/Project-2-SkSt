e5becf834634e003e3b23bbf38b7a380c577d9fe
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-06 09:01:00 -0700

    Updated data models to support auth

--- src/main/java/com/skillstorm/taxprep/controllers/ApiController.java
+++ src/main/java/com/skillstorm/taxprep/controllers/ApiController.java
@@ -1,17 +1,22 @@
 package com.skillstorm.taxprep.controllers;
 
+import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstorm.taxprep.models.AppUser;
 import com.skillstorm.taxprep.models.TaxInfo;
+import com.skillstorm.taxprep.models.TaxInfoW2;
 import com.skillstorm.taxprep.service.CalculationService;
 import com.skillstorm.taxprep.service.DatabaseService;
+import com.skillstorm.taxprep.service.UserService;
 
 @RestController
 public class ApiController {
@@ -21,40 +26,52 @@ public class ApiController {
     @Autowired
     private CalculationService cS;
 
-
-    //TODO On entering page as existing user, populate page with existing data
-
-    //TODO Use spring security to get current user, and set that userinfo to taxInfo user_id field
-    @PostMapping("/taxinfo")
-    public ResponseEntity<String> processTaxInfo(@RequestBody TaxInfo taxInfo) {
-
-        dbS.submit(taxInfo);
-                
-        return new ResponseEntity<>("TAXES OWED", HttpStatus.OK);
-    }
+    @Autowired
+    UserService userService;
 
     @GetMapping("/calculateTaxesOwed")
-    public ResponseEntity<String> calculateTaxesOwed(@RequestParam Long userId) {
-        String x = cS.calculateTaxesOwed(userId);
+    public ResponseEntity<String> calculateTaxesOwed(Authentication auth) {
+        AppUser u = (AppUser) userService.loadUserByUsername(auth.getName());
+        String x = cS.calculateTaxesOwed(u.getId());
         return new ResponseEntity<>(x, HttpStatus.OK);
     }
 
     @GetMapping("/income")
-    public ResponseEntity<Double> getIncome(@RequestParam Long userId)
+    public ResponseEntity<Double> getIncome(Authentication auth, @RequestParam Long userid)
     {
-        return ResponseEntity.ok(cS.getIncomeById(userId));
+        // AppUser u = (AppUser) userService.loadUserByUsername(auth.getName());
+
+        return ResponseEntity.ok(cS.getIncomeById(userid));
     }
 
     @GetMapping("/deductions")
-    public ResponseEntity<Double> getDeductions(@RequestParam Long userId)
+    public ResponseEntity<Double> getDeductions(Authentication auth)
     {
-        return ResponseEntity.ok(cS.getDeductionsById(userId));
+        AppUser u = (AppUser) userService.loadUserByUsername(auth.getName());
+
+        return ResponseEntity.ok(cS.getDeductionsById(u.getId()));
     }
 
     @GetMapping("/withheld")
-    public ResponseEntity<Double> getWitheld(@RequestParam Long userId)
+    public ResponseEntity<Double> getWitheld(Authentication auth)
     {
-        return ResponseEntity.ok(cS.getWithheldById(userId));
+        AppUser u = (AppUser) userService.loadUserByUsername(auth.getName());
+
+        return ResponseEntity.ok(cS.getWithheldById(u.getId()));
+    }
+
+
+    @PostMapping("/submitW2")
+    public ResponseEntity<String> submitW2(Authentication auth, @RequestBody TaxInfoW2 info)
+    {
+        AppUser u = (AppUser) userService.loadUserByUsername(auth.getName());
+        TaxInfoW2 newInfo = new TaxInfoW2(u, info);
+        // newInfo.setId(u.getId());
+        // System.out.println(newInfo);
+        // newInfo.setUser(u);
+
+        dbS.submitW2(newInfo);
+        return ResponseEntity.ok("asdf");
     }
 
 }
