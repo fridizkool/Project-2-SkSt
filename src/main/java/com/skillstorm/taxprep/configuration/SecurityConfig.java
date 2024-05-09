9ed10328e88799ccfc6d753c3ec77a3fb2d6c3a5
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-09 11:34:02 -0700

    Changed sessions back since https is now supported

--- src/main/java/com/skillstorm/taxprep/configuration/SecurityConfig.java
+++ src/main/java/com/skillstorm/taxprep/configuration/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,9 +42,7 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
-            ).sessionManagement(sessionManagement -> sessionManagement
-            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) // or .NEVER for stateless
-        );
+            );
             
 		return http.build();
 	}
