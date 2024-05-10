bdf0fc8298d954cd1d8ae523df0bf58d629feebc
Kaiwen Tsou <kktsou@ucsd.edu>
2024-05-10 07:45:44 -0700

    Removed unnecessary cors tag

--- src/main/java/com/skillstorm/taxprep/configuration/SecurityConfig.java
+++ src/main/java/com/skillstorm/taxprep/configuration/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +18,6 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-            .cors(AbstractHttpConfigurer::disable)
             .csrf((csrf) -> csrf.
                 disable())
 			.authorizeHttpRequests((authorize) -> authorize
